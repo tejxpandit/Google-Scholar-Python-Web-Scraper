f0a2f3c9acbfa5c63238cd76a5ec6de075a1c0a1
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-09-27 23:17:38 -0500

    Google Scholar Web Scraper Class
    Created the base class for the google scholar based web scraper.

---
+++ GoogleScholarScraper.py
@@ -0,0 +1,17 @@
+# Project   : Google Scholar Web Scraper in Python
+# File      : Google Scholar Scraper
+# Author    : Tej Pandit
+# Date      : Sept 2024
+
+import pickle
+from util.WebpageScraper import WebpageScraper
+from GoogleScholarParser import GoogleScholarParser
+
+# Import Data 
+from GoogleScholar_URLs import URLs
+
+class GoogleScholarScraper:
+    def __init__(self):
+        self.scraper = None
+        self.parser = None
+        self.auth_urls = URLs
