4d7e86410299c84fe8009f3b53c4f16554fb13c4
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-10-13 18:32:23 -0500

    Google Scholar Author URLs
    Added the URL file which specifies which Author URLs need to be scraped for publication data.

---
+++ GoogleScolar_URLs.py
@@ -0,0 +1,10 @@
+# Project   : Google Scholar Web Scraper in Python
+# File      : Google Scholar Author URLs to be Scraped
+# Author    : Tej Pandit
+# Date      : Oct 2024
+
+# These example URLs can be replaced with URLs you need
+URLs = {
+    "Albert Einstein" : "https://scholar.google.com/citations?user=qc6CJjYAAAAJ&hl=en&oi=ao",
+    "Geoffrey Hinton" : "https://scholar.google.com/citations?user=JicYPdAAAAAJ&hl=en"
+}
